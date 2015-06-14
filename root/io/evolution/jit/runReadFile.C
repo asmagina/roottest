77bf1abc1eba077aee0bfee0209143ad49f2c6a4
anna <a.smagina1995@gmail.com>
2015-06-14 23:23:07 +0300

    del2

--- root/io/evolution/jit/runReadFile.C
+++
@@ -1,9 +0,0 @@
-#include "readFile.C"
-
-void runReadFile()
-{
-   std::cout << "Reading test1.root\n" << std::endl;
-   readFile("test.root", "AliAODForwardMult");
-
-//   std::cout << "\n\n--------------------------------------------------\n\n" << std::endl;
-}
