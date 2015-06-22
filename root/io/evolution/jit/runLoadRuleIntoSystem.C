3274ca1b2340866af3a899dfebb87004de9cde74
anna <a.smagina1995@gmail.com>
2015-06-22 17:11:28 +0300

    Added one more testing rule

--- root/io/evolution/jit/runLoadRuleIntoSystem.C
+++ root/io/evolution/jit/runLoadRuleIntoSystem.C
@@ -13,8 +13,10 @@ void runLoadRuleIntoSystem()
    std::vector<std::string> vect;
    readRulesFromFile(f, vect); 
 
-   for (int i = 0; i < vect.size(); i++) {
-      std::cout << "Adding rule " << vect[i] << " ..." << std::endl;
-      loadRuleIntoSystem(vect[i].c_str());
-   }
+   vect.push_back("type=read sourceClass=\"AliAODForwardMult\" targetClass=\"AliAODForwardMult\" source=\"UInt_t fTriggers; Float_t fIpZ; Float_t fCentrality; UShort_t fNClusters;\" target=\"fHeader\" version=\"[-5]\" code=\"{newObj->CreateHeader(onfile.fTriggers,onfile.fIpZ,onfile.fCentrality,onfile.fNClusters);}\"");
+
+//   for (int i = 0; i < vect.size(); i++) {
+//      std::cout << "Adding rule " << vect[i] << " ..." << std::endl;
+      loadRuleIntoSystem(vect[2].c_str());
+//   }
 }
