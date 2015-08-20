c59c75935a6d3f2cb5d5ae4d578a0f2c7da27a91
anna <a.smagina1995@gmail.com>
2015-08-20 18:45:21 +0300

    Removed incorrect test

--- root/io/evolution/ReadNestedObjects.C
+++ root/io/evolution/ReadNestedObjects.C
@@ -17,41 +17,27 @@ void ReadNestedObjects()
       return;
    }
 
-   // read nested object with transformation of its field
-   cout << "[ TEST 1 ]" << endl;
    TClass::AddRule("#pragma read sourceClass=\"EventHeader\" version=\"[1]\" targetClass=\"EventHeader\" "
-                   "source=\"Int_t fDate\" target=\"fDate\" "
+                   "include=\"<iostream>;\" "
+                   "source=\"Int_t fDate;\" target=\"fDate\" "
                    "code=\"{ fDate.SetSec(onfile.fDate + 100000); fDate.SetNanoSec(100); "
                    "}\" ", kFALSE); 
+
    if ( TClass::AddRule("#pragma read sourceClass=\"Event\" version=\"[-3]\" targetClass=\"Event\" "
-                        "source=\"EventHeader fEvtHdr\" target=\"fEvtHdr\" "
-                        "code=\"{ if (onfile.fEvtHdr->GetClassVersion() == 1) {"
-                        " fFlag = onfile.fEvtHeader->GetMember<Int_t>(fDate_id) % 2; "
-                        " onfile.fEvtHdr->Load( fEvtHdr ); } }\" ", kFALSE) )
+                        "include=\"<iostream>;\" "
+                        "source=\"EventHeader fEvtHdr;\" target=\"fEvtHdr, fFlag\" "
+                        "code=\"{if (onfile.fEvtHdr->GetClassVersion() == 1) {  "
+                        "fFlag = onfile.fEvtHdr->GetMember<Int_t>(onfile.fEvtHdr->GetId(\"fDate\")) % 2; "
+                        "onfile.fEvtHdr->Load(&fEvtHdr, \"EventHeader\"); } }\" ", kFALSE) )
       cout << "[ SUCCEED ] [ As expected, rule was successfully load into system ]" << endl;
    else 
       cout << "[ FAIL ] [ Rule is expected to be successfully load into system ] " << endl;
 
-   // read a collection with transformation of its elements
-   cout << "[ TEST 2 ]" << endl;
-   TClass::AddRule("#pragma read sourceClass=\"Track\" version=\"[-2]\" targetClass=\"Track\" "
-                   "source=\"\" target=\"fNewData\" "
-                   "code=\"{ fNewData = 100500; }\" ", kFALSE); 
-
-   if ( TClass::AddRule("#pragma read sourceClass=\"Event\" version=\"[-3]\" targetClass=\"Event\" "
-                        "source=\"TClonesArray* fTracks;\" target=\"fTracks\" "
-                        "code=\"{ for (UInt_t i = 0; i < onfile.fTracks->GetSize(); i++) {"
-                        "onfile.fTracks->At(i)->Load(fTracks[i]); "
-                        "}}\" ", kFALSE) )
-      cout << "[ SUCCEED ] [ As expected, rule was successfully load into system ]" << endl;
-   else 
-      cout << "[ FAIL ] [ Rule is expected to be successfully load into system ] " << endl;
-
-/*   Event* event = new Event();
+   Event* event = new Event();
    TTree* t = (TTree*)f->Get("T");
    t->SetBranchAddress("event", &event);
    t->GetEntry(0);
    event->ls();
-*/
+
    f->Close();
 }
